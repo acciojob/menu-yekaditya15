cc5b0ec782fdaaba247cb650ead834c7401ab9bb
yekaditya11 <yekaditya11@gmail.com>
2024-12-28 20:59:47 +0530

    update2

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,47 @@
 import React, { useState } from "react";
-import DishList from "./DishList";
-import CategoryFilter from "./CategoryFilter";
-import "../styles/App.css";
-const App = () => {
-  const [category, setCategory] = useState("all");
+import data from "./data";
+
+function App() {
+  const [items, setItems] = useState(data);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setItems(data);
+    } else {
+      setItems(data.filter((item) => item.category === category));
+    }
+  };
 
   return (
-    <div className="app">
-      <h1>Dish Reviews</h1>
-      <CategoryFilter setCategory={setCategory} />
-      <DishList category={category} />
-    </div>
+    <main id="main">
+      <header>
+        <h1>Dish Reviews</h1>
+        <div>
+          <button id="filter-btn-all" onClick={() => filterItems("all")}>
+            All
+          </button>
+          <button id="filter-btn-1" onClick={() => filterItems("breakfast")}>
+            Breakfast
+          </button>
+          <button id="filter-btn-2" onClick={() => filterItems("lunch")}>
+            Lunch
+          </button>
+          <button id="filter-btn-3" onClick={() => filterItems("shakes")}>
+            Shakes
+          </button>
+        </div>
+      </header>
+      <section>
+        {items.map((item) => (
+          <article key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <p>${item.price}</p>
+          </article>
+        ))}
+      </section>
+    </main>
   );
-};
+}
 
 export default App;
